1e5b99293b8ac428571eef456e630243fd658de4
IvanGB7 <ivangutierrezbo798@gmail.com>
2022-07-05 14:46:22 -0500

    Practica 2 - Sign Up User

---
+++ widgetgallery/lib/models/gender.dart
@@ -0,0 +1,9 @@
+export 'package:widgetgallery/models/gender.dart';
+export 'package:widgetgallery/models/item_menu.dart';
+export 'package:widgetgallery/models/student.dart';
+
+enum Gender {
+  female,
+  male,
+  notSpecify,
+}
---
+++ widgetgallery/lib/models/student.dart
@@ -0,0 +1,25 @@
+export 'package:widgetgallery/models/item_menu.dart';
+export 'package:widgetgallery/models/student.dart';
+
+class Student {
+  final String firstName, lastName, phone, email;
+  final int gender;
+  final String country;
+
+  Student({
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    this.phone = '',
+    this.gender = 0,
+    this.country = '',
+  });
+
+  factory Student.empty() => Student(
+      firstName: '',
+      lastName: '',
+      email: '',
+      gender: 0,
+      country: '',
+      phone: '');
+}
